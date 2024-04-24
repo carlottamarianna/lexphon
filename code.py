b68dd23669b41e4fb014d0d21289c8e07579062b
carlotta_cascino <cascino.carlotta@gmail.com>
2024-04-24 11:35:38 +0200

    code.py

--- code.py
+++ code.py
@@ -5,10 +5,13 @@ they must press a key as quickly as possible.
 """
 
 import random
+import pandas
+import csv
 from expyriment import design, control, stimuli
 
 operatie = 'operatie.wav'
 opelakoe = 'opelakoe.wav'
+
 
 N_TRIALS = 20
 MIN_WAIT_TIME = 1000
@@ -19,6 +22,10 @@ exp = design.Experiment(name="Visual Detection", text_size=40)
 #control.set_develop_mode(on=True)
 control.initialize(exp)
 
+
+target1 = stimuli.Audio(operatie)
+target2 = stimuli.Audio(opelakoe)
+
 #words = [operatie]
 #pseudos = [opelakoe]
 
@@ -28,10 +35,12 @@ control.initialize(exp)
 #for item in pseudos:
     #trials.append(("P", item, stimuli.TextLine(item)))
 
-#random.shuffle(trials)
+##random.shuffle(trials)
 
-target = stimuli.Audio(operatie)
 blankscreen = stimuli.BlankScreen()
+
+targlist = [target1,target2] *(N_TRIALS //2)
+random.shuffle(targlist)
 
 instructions = stimuli.TextScreen("Instructions",
     f"""From time to time, you will hear a word or a non-word.
@@ -48,12 +57,20 @@ control.start(skip_ready_screen=True)
 instructions.present()
 exp.keyboard.wait()
 
-for i_trial in range(N_TRIALS):
+
+
+for i_trial in range(0,4):
     blankscreen.present()
     waiting_time = random.randint(MIN_WAIT_TIME, MAX_WAIT_TIME)
     exp.clock.wait(waiting_time)
-    target.present()
+    target3 = targlist[i_trial]
+    target3.present()
     key, rt = exp.keyboard.wait(duration=MAX_RESPONSE_DELAY)
     exp.data.add([i_trial, waiting_time, key, rt])
+    with open('lexphon_results.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['trial', 'wait', 'respkey', 'RT'])
+        writer.writerow([i_trial, waiting_time, key, rt])
+
 
 control.end()
